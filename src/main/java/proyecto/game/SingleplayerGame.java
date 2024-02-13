42071e679deecc99fa301b2e69180b612b279098
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-02-13 17:26:59 -0400

    funcion canvasResize

--- src/main/java/proyecto/game/SingleplayerGame.java
+++ src/main/java/proyecto/game/SingleplayerGame.java
@@ -1,46 +1,38 @@
 package proyecto.game;
 
-import proyecto.model.*;
 import java.util.ArrayList;
+import proyecto.model.*;
 
 public class SingleplayerGame {
 
-    private SpriteSheet spriteSheet;
-    private Level level;
-    private Player player;
-    private ArrayList<Bomb> bombs;
+  private SpriteSheet spriteSheet;
+  private Level level;
+  private Player player;
+  private ArrayList<Bomb> bombs;
 
-    public SingleplayerGame() {
-        spriteSheet = SpriteSheet.getInstance();
-        player = new Player(1, 1);
-        player.setBombType(BombType.BASIC);
-        bombs = new ArrayList<Bomb>();
-        level = new Level(15, 13, player.getCoord());
-    }
+  public SingleplayerGame() {
+    spriteSheet = SpriteSheet.getInstance();
+    player = new Player(1, 1);
+    player.setBombType(BombType.BASIC);
+    bombs = new ArrayList<Bomb>();
+    level = new Level(15, 13, player.getCoord());
+  }
 
-    public SpriteSheet getSpriteSheet() {
-        return spriteSheet;
-    }
+  public SpriteSheet getSpriteSheet() { return spriteSheet; }
 
-    public Level getLevel() {
-        return level;
-    }
+  public Level getLevel() { return level; }
 
-    public Player getPlayer() {
-        return player;
-    }
+  public Player getPlayer() { return player; }
 
-    public ArrayList<Bomb> getBombs() {
-        return bombs;
-    }
+  public ArrayList<Bomb> getBombs() { return bombs; }
 
-    public void addBomb(BombType bombType, Coord<Integer> coords) {
-        Bomb newBomb = new BasicBomb(coords);
+  public void addBomb(BombType bombType, Coord<Integer> coords) {
+    Bomb newBomb = new BasicBomb(coords);
 
-        // if (bombType == BombType.BASIC) {
-        // newBomb = new BasicBomb(coords);
-        // }
+    // if (bombType == BombType.BASIC) {
+    // newBomb = new BasicBomb(coords);
+    // }
 
-        bombs.add(newBomb);
-    }
+    bombs.add(newBomb);
+  }
 }
